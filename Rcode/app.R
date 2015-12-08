17d18c9373807c90cd9b7e0e6d5086c2ffce41f7
SoffiaI <soffia.ingibjargar@gmail.com>
2015-12-08 11:03:14 +0000

    shiny

--- Rcode/app.R
+++ Rcode/app.R
@@ -1,42 +1,47 @@
-# 01-kmeans-app
-
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-          "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+# 05-tabs.R
 
 library(shiny)
 
-ui <- fluidPage(
-  headerPanel('Iris k-means clustering'),
-  sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-                selected = names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 9)
-  ),
-  mainPanel(
-    plotOutput('plot1')
-  )
+ui <- fluidPage(title = "Random generator",
+                tabsetPanel(              
+                  tabPanel(title = "Normal data",
+                           plotOutput("norm"),
+                           actionButton("renorm", "Resample")
+                  ),
+                  tabPanel(title = "Uniform data",
+                           plotOutput("unif"),
+                           actionButton("reunif", "Resample")
+                  ),
+                  tabPanel(title = "Chi Squared data",
+                           plotOutput("chisq"),
+                           actionButton("rechisq", "Resample")
+                  )
+                )
 )
 
 server <- function(input, output) {
   
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
+  rv <- reactiveValues(
+    norm = rnorm(500), 
+    unif = runif(500),
+    chisq = rchisq(500, 2))
   
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
+  observeEvent(input$renorm, { rv$norm <- rnorm(500) })
+  observeEvent(input$reunif, { rv$unif <- runif(500) })
+  observeEvent(input$rechisq, { rv$chisq <- rchisq(500, 2) })
   
-  output$plot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  output$norm <- renderPlot({
+    hist(rv$norm, breaks = 30, col = "grey", border = "white",
+         main = "500 random draws from a standard normal distribution")
   })
-  
+  output$unif <- renderPlot({
+    hist(rv$unif, breaks = 30, col = "grey", border = "white",
+         main = "500 random draws from a standard uniform distribution")
+  })
+  output$chisq <- renderPlot({
+    hist(rv$chisq, breaks = 30, col = "grey", border = "white",
+         main = "500 random draws from a Chi Square distribution with two degree of freedom")
+  })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(server = server, ui = ui)
